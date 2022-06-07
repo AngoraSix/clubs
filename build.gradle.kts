09def6d22b097dd04eaf00993f0ab4ea48f0fae1
Ger Roza <rozagerardo@gmail.com>
2022-06-06 21:14:07 -0300

    cleaned HELP autogenerated file (initializr), jenv file, and HATEOAS dependency (not used, and brought issued, autoconfiguring servlet MVC stack by default)

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,6 @@ val snippetsDir by extra { file("build/generated-snippets") }
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-//	implementation("org.springframework.boot:spring-boot-starter-hateoas")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
