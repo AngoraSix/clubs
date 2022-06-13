67ffa4a2ae6070b4c9da43bf20f87989d5037897
Ger Roza <rozagerardo@gmail.com>
2022-06-13 16:39:30 -0300

    Fixed transient isProjectAdmin Member field getting persisted

--- src/main/kotlin/com/angorasix/clubs/domain/club/Member.kt
+++ src/main/kotlin/com/angorasix/clubs/domain/club/Member.kt
@@ -1,16 +1,26 @@
 package com.angorasix.clubs.domain.club
 
+import org.springframework.data.annotation.PersistenceCreator
+
 /**
  * <p>
  * </p>
  *
  * @author rozagerardo
  */
-data class Member(
-        var contributorId: String,
-        var roles: Collection<String> = mutableSetOf(),
-        var isProjectAdmin: Boolean = false
+data class Member constructor(
+        val contributorId: String,
+        val roles: Collection<String> = mutableSetOf(),
+        @field:org.springframework.data.annotation.Transient val isProjectAdmin: Boolean = false
 ) {
+
+
+    @PersistenceCreator
+    private constructor(
+            contributorId: String,
+            roles: Collection<String> = mutableSetOf()
+    ) : this(contributorId, roles, false)
+
     override fun equals(other: Any?): Boolean =
             other is Member && other.contributorId == contributorId
 
